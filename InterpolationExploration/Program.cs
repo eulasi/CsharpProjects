7ca67967fe1f6b6ff87401338a11cad6a0d3e625
EUlasi <96310496+eulasi@users.noreply.github.com>
2024-01-06 14:11:26 -0500

    Currency Formatting Lessons

---
+++ InterpolationExploration/Program.cs
@@ -0,0 +1,40 @@
+﻿int invoiceNumber = 1201;
+decimal productShares = 25.4568m;
+decimal subtotal = 2750.00m;
+decimal taxPercentage = .15825m;
+decimal total = 3185.19m;
+// Display the incoive number using string interpolation
+Console.WriteLine($"Invoice Number: {invoiceNumber}");
+
+// Display the product shares with one thousandth of a share (0.001) precision
+Console.WriteLine($"    Shares: {productShares:N3} Product");
+// Display the subtotal that you charge the customer formatted as currency
+Console.WriteLine($"        Sub Total: {subtotal:C}");
+// Display the tax charged on the sale formatted as a percentage
+Console.WriteLine($"            Tax: {taxPercentage:P2}");
+// Finalize the receipt with the total amount due formatted as currency
+Console.WriteLine($"        Total Billed: {total:C}");
+
+// Padding and Alignment Intermission
+string input = "Pad this";
+Console.WriteLine(input.PadLeft(12));
+Console.WriteLine(input.PadRight(12));
+
+// Overloaded Method
+Console.WriteLine(input.PadLeft(12, '-'));
+Console.WriteLine(input.PadRight(12, '-'));
+
+// Adding payment ID to the output
+string paymentId = "769C";
+// Add the payee name to the output
+string payeeName = "Mr. Stephen Ortega";
+// Add the payment amount to the output
+string paymentAmount = "$5,000.00";
+var formattedLine = paymentId.PadRight(6);
+formattedLine += payeeName.PadRight(24);
+formattedLine += paymentAmount.PadLeft(10);
+
+Console.WriteLine("1234567890123456789012345678901234567890");
+Console.WriteLine(formattedLine);
+
+
---
+++ StringFormattingBasics/Program.cs
@@ -0,0 +1,42 @@
+﻿// Composite Formatting
+string first = "Hello";
+string second = "World";
+string result = string.Format("{0} {1}!", first, second);
+Console.WriteLine(result);
+
+// Example Two
+Console.WriteLine("{1} {0}!", first, second);
+Console.WriteLine("{0} {0} {0}!", first, second);
+
+// String Interpolation
+Console.WriteLine($"{first} {second}!");
+Console.WriteLine($"{second} {first}!");
+Console.WriteLine($"{first} {first} {first}!");
+
+// Formatting Currency
+decimal price = 123.45m;
+int discount = 50;
+// add C: for the dollar sign 
+Console.WriteLine($"Price: {price:C} (Save {discount:C})");
+
+// Formatting Numbers
+decimal measurement = 123456.78912m;
+// First Two Digits after the decimal point
+Console.WriteLine($"Mesaurement: {measurement:N} units");
+// First Four Digits after the decimal point
+Console.WriteLine($"Measurement: {measurement:N4} units");
+
+// Formatting percentages
+decimal tax = .36785m;
+// P2 for percent sign
+Console.WriteLine($"Tax rate: {tax:P2}");
+
+// Combining formatting approaches
+decimal priceTwo = 67.55m;
+decimal salePrice = 59.99m;
+
+string yourDiscount = String.Format("You saved {0:C2} off the regular {1:C2} price. ", (priceTwo - salePrice), priceTwo);
+
+yourDiscount += $"A discount of {((price - salePrice)/price):P2}!"; // inserted
+Console.WriteLine(yourDiscount);
+
